492224395a546276423d061ab9e75da155bb1322
RafaelMontalvao <rushbig@hotmail.com>
2023-02-17 17:48:52 -0300

    finalizando ex06

---
+++ ex06/calculadora.js
@@ -0,0 +1,18 @@
+
+import soma from './soma.js';
+import subtracao from './subtracao.js';
+
+
+
+class Calculadora{
+    
+    somar(a,b){
+    return soma
+    }  
+    subtrair(a,b){
+    return subtracao
+}}
+
+//export default Calculadora;
+export {soma};
+export {subtracao};
---
+++ ex06/main.js
@@ -0,0 +1,7 @@
+//import calculadora from './calculadora.js'
+import {soma} from'./calculadora.js';
+import {subtracao} from './calculadora.js';
+
+soma(2, 2);
+subtracao(2,2);
+
